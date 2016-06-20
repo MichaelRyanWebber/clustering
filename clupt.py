94c3f36d03600c8ef04e451107a43054d3bb148f
MichaelRyanWebber <MichaelRyanWebber@gmail.com>
2016-06-20 17:53:42 -0400

    added graphing of test results using plotpy
    fixed a cluster algorithm in cases were clusters
    should be joined, added joinClu() function

---
+++ clupt.py
@@ -0,0 +1,14 @@
+
+
+
+class clupt:
+    x = 0
+    y = 0
+    cluId = -1
+
+    def inClu(self):
+        if self.cluId == -1:
+            return False
+        else:
+            return True
+
--- nearbyX.py
+++ nearbyX.py
@@ -6,17 +6,42 @@
 
 from scipy.spatial import distance
 
+import plotly
+from plotly.graph_objs import Scatter, Layout
+
 
 def testMat():
     mat = list()
-    for i in range(200):
-        for j in range(200):
-            if i % 4 == 0:
+    for i in range(40):
+        for j in range(20):
+            if (i % 4 == 0) and (j % 10 != 0):
                 mat.append([[i, j], -1])
+            elif j % 8 == 0:
+                mat.append([[i, j], -1])
+
+    mat.append([[3.54,8.9],-1])
+    # mat.append([[3.54,8.9],-1])
+
+
 
     del mat[4:6]
 
     return mat
+
+
+# join the clusters specified by ids
+# expects ids as a set
+def joinClusters(clusters, ids):
+    lids = list(ids)
+    for index in lids[1:]:
+        clusters[lids[0]].extend(clusters[index])
+
+    for pt in clusters[lids[0]]:
+        pt[1] = lids[0]
+
+    for index in lids[1:]:
+        clusters[index] = []
+
 
 # cluster points that are within X of each other
 def clusterNNX(mat,X):
@@ -24,23 +49,29 @@ def clusterNNX(mat,X):
     clindex = 0
     clusters.append([mat[0]])
     mat[0][1] = clindex
-    for pt1 in mat[1:]:
+    for pt1 in mat:
 
         # first check if this is a neighbor to an existing cluster
+        joinIds = set()
         for aclustr in clusters:
             for pt2 in aclustr:
-                if distance.euclidean(pt1[0], pt2[0]) < X:
-                    pt1[1] = pt2[1]
-                    aclustr.append(pt1)
-                    break
-            if pt1[1] != -1:
-                break
+                if distance.euclidean(pt1[0], pt2[0]) < X and pt1 != pt2:
+                    if len(joinIds) == 0:
+                        pt1[1] = pt2[1]
+                        aclustr.append(pt1)
+                        joinIds.add(pt1[1])
+                    else:
+                        joinIds.add(pt2[1])
+
+        # see if this point should join 2 or more existing clusters
+        if len(joinIds) > 1:
+            joinClusters(clusters, joinIds)
 
         # see if pt1 has been put into a cluster yet
         # if not, check the remaining points
         if pt1[1] == -1:
             for pt2 in mat:
-                if (distance.euclidean(pt1[0], pt2[0]) < X) & (pt1 != pt2):
+                if (distance.euclidean(pt1[0], pt2[0]) < X) and (pt1 != pt2):
                     clindex += 1
                     pt1[1] = clindex
                     pt2[1] = clindex
@@ -59,9 +90,25 @@ def clusterNNX(mat,X):
 
 # execute test code for above functions
 
-clusters = clusterNNX(testMat(),2)
+clusters = clusterNNX(testMat(), 2)
+x = list()
+y = list()
+traceList = list()
 
 for aclustr in clusters:
-    print aclustr
+    for pt in aclustr:
+        print pt[0],
+        x.append(pt[0][0])
+        y.append(pt[0][1])
+    traceList.append(Scatter(x=x,y=y,mode='markers'))
+    x = list()
+    y = list()
+    print
+
+
+# plotly.offline.plot({"data": [Scatter(x,y)], "layout": Layout(title="hello world")})
+# plotly.offline.plot([Scatter(x,y)])
+
+plotly.offline.plot(traceList)
 
 

be43e12dec7966dced811a58c12dc305ced9cd9a
MichaelRyanWebber <MichaelRyanWebber@gmail.com>
2016-06-23 16:32:13 -0400

    implemented a bucket based presort (or hash) to greatly limit the number of pts that need to be compared for neighbors. Brings the algorithm down to O(kN) with k being a function of the density of clustering and the maxRadius for grouping

--- nearbyX.py
+++ nearbyX.py
@@ -9,27 +9,28 @@ from scipy.spatial import distance
 import plotly
 from plotly.graph_objs import Scatter, Layout
 from cluPt import *
+from decimal import Decimal
 
 
 def testMat(returnPtObjs=False):
     mat = list()
 
-    for i in range(200):
-        for j in range(200):
+    for i in range(2000):
+        for j in range(2000):
             if (i % 4 == 0) and (j % 10 != 0):
                 mat.append([[i, j], -1])
             elif j % 8 == 0:
                 mat.append([[i, j], -1])
 
-    mat.append([[3.54, 8.9], -1])
-    mat.append([[6.7, 32.1], -1])
+    # mat.append([[3.54, 8.9], -1])
+    # mat.append([[6.7, 32.1], -1])
 
     del mat[4:6]
 
     if returnPtObjs == True:
         matclupt = list()
-        for pt in mat:
-            matclupt.append(cluPt(pt[0][0], pt[0][1]))
+        for index, pt in enumerate(mat):
+            matclupt.append(cluPt(pt[0][0], pt[0][1], index))
         return matclupt
 
     return mat
@@ -95,23 +96,106 @@ def clusterNNX(mat, X):
     return clusters
 
 
-# execute test code for above functions
-clusters = clusterNNX(testMat(True), 2)
-x = list()
-y = list()
-traceList = list()
+# cluster but use a bucket grid to limit the search for NN
+# bucket size is dependent on max radius X
+def bucskterNNX(mat, X):
+    # clusters is list of lists of cluPts
+    clusters = list()
+    clindex = 0
 
-for aclustr in clusters:
-    for pt in aclustr:
-        print pt.xy(),
-        x.append(pt.x)
-        y.append(pt.y)
-    traceList.append(Scatter(x=x, y=y, mode='markers'))
+    # buckets contains a sparse array of pts based on their
+    # proximity, implemented as a dict keyed on coordinates
+    buckets = Buckets(X)
+    buckets.makeBuckets(mat)
+    bucketsclu = Buckets(X)
+
+    # initialize the array of clusters
+    # putting the 1st point in the 1st cluster
+    clusters.append([mat[0]])
+    mat[0].cluId = clindex
+    buckets.remove(mat[0])
+    bucketsclu.add(mat[0])
+
+    # loop through each point, finding its cluster
+    for pt1 in mat[1:]:
+
+        neighborkeys = buckets.getNeighborKeys(pt1)
+
+        joinIds = set()
+
+        # check if this point was already added to a cluster
+        if pt1.inClu(): continue
+
+        # first check if this is a neighbor to an existing cluster
+        for key in neighborkeys:
+            # check if this bucket has clustered points
+            for pt2 in bucketsclu.getBucket(key):
+                if distance.euclidean(pt1.xy(), pt2.xy()) < X and pt1 != pt2:
+                    if len(joinIds) == 0:
+                        pt1.cluId = pt2.cluId
+                        clusters[pt1.cluId].append(pt1)
+                        joinIds.add(pt1.cluId)
+                        # switch to bucketsclu for this point
+                        buckets.remove(pt1)
+                        bucketsclu.add(pt1)
+                    else:
+                        joinIds.add(pt2.cluId)
+
+        # see if this point should join 2 or more existing clusters
+        if len(joinIds) > 1:
+            joinClusters(clusters, joinIds)
+
+        # see if pt1 has been put into a cluster yet
+        # if not, check the remaining points
+        if pt1.inClu(): continue
+
+        for key in neighborkeys:
+            # check if this bucket has clustered points
+            for pt2 in buckets.getBucket(key):
+                if distance.euclidean(pt1.xy(), pt2.xy()) < X and pt1 != pt2:
+                    clindex += 1
+                    pt1.cluId = clindex
+                    pt2.cluId = clindex
+                    clusters.append([pt1, pt2])
+                    # switch to bucketsclu for these two points
+                    buckets.remove(pt1)
+                    buckets.remove(pt2)
+                    bucketsclu.add(pt1)
+                    bucketsclu.add(pt2)
+                    break
+
+        # if still not in a cluster, we know this pt is
+        # a singleton cluster
+        if not pt1.inClu():
+            clindex += 1
+            pt1.cluId = clindex
+            clusters.append([pt1])
+            buckets.remove(pt1)
+            bucketsclu.add(pt1)
+
+    return clusters
+
+
+# testing code when run as main
+if __name__ == "__main__":
+    # execute test code for above functions
+    # clusters = clusterNNX(testMat(True), 2)
+    clusters = bucskterNNX(testMat(True), 2)
     x = list()
     y = list()
-    print
+    traceList = list()
 
-# plotly.offline.plot({"data": [Scatter(x,y)], "layout": Layout(title="hello world")})
-# plotly.offline.plot([Scatter(x,y)])
+    for aclustr in clusters:
+        for pt in aclustr:
+            print pt.xy(),
+            x.append(pt.x)
+            y.append(pt.y)
+        traceList.append(Scatter(x=x, y=y, mode='markers'))
+        x = list()
+        y = list()
+        print
 
-plotly.offline.plot(traceList)
+    # plotly.offline.plot({"data": [Scatter(x,y)], "layout": Layout(title="hello world")})
+    # plotly.offline.plot([Scatter(x,y)])
+
+    plotly.offline.plot(traceList)

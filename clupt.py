89831eb47bf946fe0fbc721e87924d6c9972e88e
Albert Carter <albert.r.carter@gmail.com>
2016-07-10 23:09:50 -0400

    pep8, import geopy

--- clupt.py
+++ clupt.py
@@ -1,5 +1,14 @@
+import geopy
+
 
 class CluPt:
+    def __init__(self, x, y, index):
+        self.x = x
+        self.y = y
+        self.cluId = -1
+        self.originId = index
+        return
+
     def inClu(self):
         if self.cluId == -1:
             return False
@@ -9,48 +18,45 @@ class CluPt:
     def xy(self):
         return tuple([self.x, self.y])
 
-    def __init__(self, x, y, index):
-        self.x = x
-        self.y = y
-        self.cluId = -1
-        self.originId = index
-        return
-
 
 # buckets of cluster points:
 # a sparse array of lists implemented by dict keyed on xy-coord
 # bucket size is defined by maxRadius (distance within which to group points)
 class Buckets:
-    # this is the hash used to assign buckets, floor division then multiplication by maxRadius
-    def getKey(self, pt):
-        return tuple([pt.x // self.maxRadius * self.maxRadius, pt.y // self.maxRadius * self.maxRadius])
-
-    # return a list of keys for this bucket and the 8 adjacent buckets
-    # does no bounds checking, as all access methods are safe to bounds
-    def getNeighborKeys(self,pt):
-        middle = self.getKey(pt)
-        keys = list()
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                keyX = middle[0]+(i*self.maxRadius)
-                keyY = middle[1]+(j*self.maxRadius)
-                keys.append(tuple([keyX, keyY]))
-        return keys
-
-    # fill buckets, implemented as dict keyed on
-    # the tuple of [x , y] coords, containing a list CluPts
-    def makeBuckets(self,mat):
-        for pt in mat:
-            self.add(pt)
 
     # initialize a Buckets object, defined by maxRadius
     def __init__(self, X):
         self.maxRadius = X
         self.mBuckets = dict()
 
+    # this is the hash used to assign buckets, floor division then
+    # multiplication by maxRadius
+    def getKey(self, pt):
+        return tuple(
+            [pt.x // self.maxRadius * self.maxRadius,
+             pt.y // self.maxRadius * self.maxRadius])
+
+    # return a list of keys for this bucket and the 8 adjacent buckets
+    # does no bounds checking, as all access methods are safe to bounds
+    def getNeighborKeys(self, pt):
+        middle = self.getKey(pt)
+        keys = list()
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                keyX = middle[0] + (i * self.maxRadius)
+                keyY = middle[1] + (j * self.maxRadius)
+                keys.append(tuple([keyX, keyY]))
+        return keys
+
+    # fill buckets, implemented as dict keyed on
+    # the tuple of [x , y] coords, containing a list CluPts
+    def makeBuckets(self, mat):
+        for pt in mat:
+            self.add(pt)
+
     # get a bucket: an always iterable safe bucket retrieve
     def getBucket(self, key):
-        return self.mBuckets.get(key,range(0))
+        return self.mBuckets.get(key, range(0))
 
     # add a point to the appropriate bucket
     def add(self, pt):
